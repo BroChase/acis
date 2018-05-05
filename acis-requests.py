2b8fd1af1dd60debaad7b8324fdfae1a7c0da84c
Chase Brown <chaseb686@gmail.com>
2018-05-05 06:37:46 -0600

    updated for single year use

--- acis-requests.py
+++ acis-requests.py
@@ -1,106 +1,41 @@
-import urllib3
-import numpy as np
-import regex as re
-import json
-import pandas as pd
+import datacollection
 
 
-def getRange(start, end, FIPS):
-    base_url = 'http://data.rcc-acis.org/MultiStnData'
-    http = urllib3.PoolManager()
-    params = {
-        "county": FIPS,
-        "sdate": start,
-        "edate": end,
-        "elems": "pcpn,avgt"
-    }
-    r = http.request('GET', base_url, fields=params)
-    p = json.loads(r.data.decode('utf-8'))
-    preciplist = []
-    templist = []
-    for i, data in p.items():
-        for m in data:
-            df = pd.DataFrame(m['data'])
-            ser = df.to_dict()
-            for k, value in ser.items():
-                if k == 0:
-                    preciplist.append(value)
-                else:
-                    templist.append(value)
+if __name__ == '__main__':
 
-    df = pd.DataFrame(preciplist)
-    df2 = pd.DataFrame(templist)
-    # replace missing M variables with zeros
-    df.replace('M', np.nan, inplace=True)
-    df2.replace('M', np.nan, inplace=True)
+    # Washington
+    # Kittatas 53037
+    # Chelan 53007
+    # Pierce 53053
+    # Yakima 53077
+    # Watcom 53073
+    # Clallam 53009
+    # Okanogan 53047
+    # ['53037', '53007', '53053', '53077', '53073', '53009', '53047']
 
-    df.replace('S', np.nan, inplace=True)
-    df2.replace('S', np.nan, inplace=True)
-    # replace missing T variables with zeros
-    df.replace('T', np.nan, inplace=True)
-    df2.replace('T', np.nan, inplace=True)
+    # Montana
+    # Gallatin 30031
+    # Jefferson 30043
+    # Beverhead 30001
+    # Ravalli 30081
+    # Mineral 30061
+    # Sanders 30087
+    # Broadwater 30007
+    # Lewis and Clark 30049
+    # ['30031', '30043', '30001', '30081', '30061', '30087', '30007']
 
-    df.replace(['A', '\*'], value='0', regex=True, inplace=True)
-    df2.replace(['A', '\*'], value='0', regex=True, inplace=True)
+    # Colorado
+    # ['08113', '08091', '08111', '08067', '08053', '08079','08109', '08049', '08037', '08051','08065', '08097', '08117',
+    # '08107', '08057', '08069', '08013', '08019']
 
+    FIPS_ids = ['08113', '08091', '08111', '08067', '08053', '08079', '08109', '08049', '08037', '08051','08065', '08097',
+                '30031', '30043', '30001', '30081', '30061', '30087', '30007', '08107', '08057', '08069', '08013', '08019',
+                '08117', '53037', '53007', '53053', '53077', '53073', '53009', '53047']
 
-    avg = []
-    avg2 =[]
-    # find the regions average precipitation per day
-    for i in range(df.shape[1]):
-        df[i] = df[i].astype('float64')
-        nantotal = df[i].isnull().sum()
-        total = df[i].sum()
-        avg.append(total/(df.shape[0]-nantotal))
-    for i in range(df2.shape[1]):
-        df2[i] = df2[i].astype('float64')
-        nantotal = df2[i].isnull().sum()
-        total = df2[i].sum()
-        avg2.append(total/(df.shape[0]-nantotal))
-    return avg, avg2
+    # To collecting span years
+    # startdate = 1990
+    # 'how many years to collect after startdate
+    # years = 26
+    #datacollection.multi_year_data(FIPS_ids, startdate, years)
 
-
-# Washington
-# Kittatas 53037
-# Chelan 53007
-# Pierce 53053
-# Yakima 53077
-# Watcom 53073
-# Clallam 53009
-# Okanogan 53047
-# ['53037', '53007', '53053', '53077', '53073', '53009', '53047']
-
-# Montana
-# Gallatin 30031
-# Jefferson 30043
-# Beverhead 30001
-# Ravalli 30081
-# Mineral 30061
-# Sanders 30087
-# Broadwater 30007
-# Lewis and Clark 30049
-# ['30031', '30043', '30001', '30081', '30061', '30087', '30007']
-
-# Colorado
-# '08113', '08091', '08111', '08067', '08053', '08079','08109', '08049', '08037', '08051','08065', '08097', '08117',
-# '08107', '08057', '08069', '08013', '08019'
-
-FIPS_ids = ['53037', '53007', '53053', '53077', '53073', '53009', '53047']
-
-for c in FIPS_ids:
-    L1 = []
-    L2 = []
-    FIPS = c
-    sdate = 1990
-    for i in range(26):
-        startdate = str(sdate+i) + '-1-1'
-        enddate = str(sdate+i) + '-12-31'
-        Plist, Tlist = getRange(startdate, enddate, FIPS)
-        L1.append(Plist)
-        L2.append(Tlist)
-
-
-    df = pd.DataFrame.from_records(L1)
-    df2 = pd.DataFrame.from_records(L2)
-    df.to_csv(FIPS+'precip.csv')
-    df2.to_csv(FIPS+'temp.csv')
+    datacollection.single_year_data(FIPS_ids)
---
+++ datacollection.py
@@ -0,0 +1,104 @@
+import urllib3
+import numpy as np
+import regex as re
+import json
+import pandas as pd
+
+def getRange(start, end, FIPS):
+
+    # url for acis multiyear data
+    base_url = 'http://data.rcc-acis.org/MultiStnData'
+    http = urllib3.PoolManager()
+    params = {
+        "county": FIPS,
+        "sdate": start,
+        "edate": end,
+        "elems": "pcpn,avgt"
+    }
+    # request the data GET
+    r = http.request('GET', base_url, fields=params)
+    # decode the json
+    p = json.loads(r.data.decode('utf-8'))
+    # to hold precipitation
+    preciplist = []
+    # to hold temperatures
+    templist = []
+    # iterate the items in the json to extract the precipitations and temps
+    for i, data in p.items():
+        for m in data:
+            df = pd.DataFrame(m['data'])
+            ser = df.to_dict()
+            for k, value in ser.items():
+                if k == 0:
+                    preciplist.append(value)
+                else:
+                    templist.append(value)
+    # create a dataframe from the precipitation and one for the temps
+    df = pd.DataFrame(preciplist)
+    df2 = pd.DataFrame(templist)
+    # replace missing M'missing' variables with zeros
+    df.replace('M', np.nan, inplace=True)
+    df2.replace('M', np.nan, inplace=True)
+    # replace S'sparse' variables with zeros
+    df.replace('S', np.nan, inplace=True)
+    df2.replace('S', np.nan, inplace=True)
+    # replace missing T'trace' variables with zeros
+    df.replace('T', np.nan, inplace=True)
+    df2.replace('T', np.nan, inplace=True)
+    # replace A 'appended' leap year variable
+    df.replace(['A', '\*'], value='0', regex=True, inplace=True)
+    df2.replace(['A', '\*'], value='0', regex=True, inplace=True)
+
+    avg = []
+    avg2 =[]
+    # find the regions average precipitation per day
+    for i in range(df.shape[1]):
+        df[i] = df[i].astype('float64')
+        nantotal = df[i].isnull().sum()
+        total = df[i].sum()
+        avg.append(total/(df.shape[0]-nantotal))
+    for i in range(df2.shape[1]):
+        df2[i] = df2[i].astype('float64')
+        nantotal = df2[i].isnull().sum()
+        total = df2[i].sum()
+        avg2.append(total/(df.shape[0]-nantotal))
+    return avg, avg2
+
+
+def multi_year_data(FIPS_ids, startdate, years):
+    for c in FIPS_ids:
+        # L1= list for precipitation row=a year columns=average per day 365 days
+        L1 = []
+        # L2= list for temps row= a year columns= average per day 365 days
+        L2 = []
+        FIPS = c
+        sdate = startdate
+        for i in range(years):
+            startdate = str(sdate + i) + '-1-1'
+            enddate = str(sdate + i) + '-12-31'
+            Plist, Tlist = getRange(startdate, enddate, FIPS)
+            L1.append(Plist)
+            L2.append(Tlist)
+
+        df = pd.DataFrame.from_records(L1)
+        df2 = pd.DataFrame.from_records(L2)
+        df.to_csv(FIPS + 'precip.csv')
+        df2.to_csv(FIPS + 'temp.csv')
+
+
+def single_year_data(FIPS_ids):
+
+    for c in FIPS_ids:
+        FIPS = c
+        L1 = []
+        L2 = []
+        startdate = '2017-1-1'
+        enddate = '2017-12-31'
+        Plist, Tlist = getRange(startdate, enddate, FIPS)
+        L1.append(Plist)
+        L2.append(Tlist)
+
+        df = pd.DataFrame.from_records(L1)
+        df2 = pd.DataFrame.from_records(L2)
+        df.to_csv(FIPS + 'precip.csv')
+        df2.to_csv(FIPS + 'temp.csv')
